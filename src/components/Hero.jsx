8a337c18813360ae495e3fc42d59c506d6306802
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2024-11-30 22:29:20 +0530

    initial frontend

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Caraousel from './Caraousel';
+
+const Hero = () =>{
+    return (
+        <div> 
+            <Caraousel/>    
+        </div>
+);
+}
+
+export default Hero;
