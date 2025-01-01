57c72530e91319747b2b630dc0c35f59c05c2db7
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2025-01-01 21:11:19 +0530

    fix: sidebar of admin page and creating a structure of the layout

--- src/components/dashboardComponents/Sidebar.jsx
+++ src/components/dashboardComponents/Sidebar.jsx
@@ -138,39 +138,29 @@ function Sidebar(props) {
       >
         <Toolbar />
         <div className="flex justify-between">
-          <Card sx={{ marginBottom: 2 }}>
+          <Card variant="outlined" sx={{ marginBottom: 2 }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
             dolor purus non enim praesent elementum facilisis leo vel. Risus at
             ultrices mi tempus imperdiet.
           </Card>
-          <Card sx={{ marginBottom: 2 }}>
+          <Card variant="outlined" sx={{ marginBottom: 2 }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
             dolor purus non enim praesent elementum facilisis leo vel. Risus at
             ultrices mi tempus imperdiet.
           </Card>
-          <Card sx={{ marginBottom: 2 }}>
+          <Card variant="outlined" sx={{ marginBottom: 2 }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
             dolor purus non enim praesent elementum facilisis leo vel. Risus at
             ultrices mi tempus imperdiet.
           </Card>
         </div>
-        <Typography sx={{ marginBottom: 2 }}>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <div className="flex justify-between">
+            <Card variant="outlined">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident, nesciunt!</Card>
+            <Card variant="outlined">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident, nesciunt!</Card>
+        </div>       
       </Box>
     </Box>
   );
