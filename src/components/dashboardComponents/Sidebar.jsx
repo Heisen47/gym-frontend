53673fed3ff65a9ac6c7b45a1541ad4c1e000163
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2025-01-02 14:02:24 +0530

    fixed sidebar

--- src/components/dashboardComponents/Sidebar.jsx
+++ src/components/dashboardComponents/Sidebar.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import GroupIcon from "@mui/icons-material/Group";
+import PaymentIcon from "@mui/icons-material/Payment";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -9,20 +12,21 @@ const Sidebar = () => {
 
   return (
     <div className="flex items-center">
-
       <div
         className={`bg-gray-800 text-white h-screen flex flex-col transition-all duration-300 ${
           isOpen ? "w-64" : "w-16"
         }`}
       >
-        <div className="flex flex-col items-center mt-10"> 
+        <div className="flex flex-col items-center mt-10">
           {/* Menu Items */}
           <div
             className={`flex items-center gap-4 w-full p-3 hover:bg-gray-700 rounded-md ${
               isOpen ? "justify-start" : "justify-center"
             }`}
           >
-            <span className="text-xl">⊞</span>
+            <span className="text-xl">
+              <DashboardIcon />{" "}
+            </span>
             {isOpen && <span className="text-lg">Dashboard</span>}
           </div>
           <div
@@ -30,7 +34,10 @@ const Sidebar = () => {
               isOpen ? "justify-start" : "justify-center"
             }`}
           >
-            <span className="text-xl">👥</span>
+            <span className="text-xl">
+              {" "}
+              <GroupIcon />{" "}
+            </span>
             {isOpen && <span className="text-lg">Customers</span>}
           </div>
           <div
@@ -38,24 +45,22 @@ const Sidebar = () => {
               isOpen ? "justify-start" : "justify-center"
             }`}
           >
-            <span className="text-xl">$</span>
+            <span className="text-xl">
+              <PaymentIcon />
+            </span>
             {isOpen && <span className="text-lg">Payment</span>}
           </div>
         </div>
       </div>
 
-      
       {/* Drawer Toggle Button */}
 
-        <button
-          onClick={toggleSidebar}
-          className="text-center bg-gray-800 text-white p-2 rounded-md cursor-pointer"
-        >
-          
-          {!isOpen ? "❮" : "❯"}
-        </button>
-
-
+      <button
+        onClick={toggleSidebar}
+        className="text-center bg-gray-800 text-white p-2 rounded-md cursor-pointer"
+      >
+        {!isOpen ? "❮" : "❯"}
+      </button>
     </div>
   );
 };
