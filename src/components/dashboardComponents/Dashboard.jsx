7129f37abbcc0598c2f8f87815d38478375ec402
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2025-01-26 22:08:02 +0530

    fix: add graphing

--- src/components/dashboardComponents/Dashboard.jsx
+++ src/components/dashboardComponents/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useMemo } from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
 import { Button } from "@mui/material";
 import CustomModal from "./Modal";
+import { LineChart } from "@mui/x-charts/LineChart";
 
 const Dashboard = () => {
   const [open, setOpen] = useState(false);
@@ -20,19 +21,32 @@ const Dashboard = () => {
         </div>
 
         <h2 className="text-xl font-semibold mb-3">User Activity</h2>
-        <PieChart
-          series={[
-            {
-              data: [
-                { id: 0, value: 10, label: "series A" },
-                { id: 1, value: 15, label: "series B" },
-                { id: 2, value: 20, label: "series C" },
-              ],
-            },
-          ]}
-          width={400}
-          height={200}
-        />
+        <div className="flex space-x-5">
+          <PieChart
+            series={[
+              {
+                data: [
+                  { id: 0, value: 10, label: "series A" },
+                  { id: 1, value: 15, label: "series B" },
+                  { id: 2, value: 20, label: "series C" },
+                ],
+              },
+            ]}
+            width={400}
+            height={200}
+          />
+
+          <LineChart
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            series={[
+              {
+                data: [2, 5.5, 2, 8.5, 1.5, 5],
+              },
+            ]}
+            width={500}
+            height={300}
+          />
+        </div>
       </div>
 
       {/* Modal for creating new customer */}
